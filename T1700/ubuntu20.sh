840f10708bc8fe79738d4da04f47c9b4f78c28c8
AreteQin <qinqiaomeng@outlook.com>
2022-01-30 14:40:58 -0500

    glog

--- T1700/ubuntu20.sh
+++ T1700/ubuntu20.sh
@@ -15,7 +15,7 @@ sudo apt install code -y
 sudo apt-get install ibus-libpinyin -y
 ibus-daemon -d -x -r
 sudo apt install vim libreoffice texlive-full -y
-sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential vlc -y
+sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential vlc libgoogle-glog-dev -y
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
