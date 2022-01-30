3ece4eaddd19be30dbec599e4432079fe7571ff7
qin <qinqiaomeng@outlook.com>
2022-01-30 11:23:12 -0500

    new

--- config_badslam_0.1.sh
+++ config_badslam_0.1.sh
@@ -1,6 +1,5 @@
 sudo apt update -y
 sudo apt upgrade -y
-
 sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential -y
 
 sudo apt-get install ros-noetic-realsense2-camera -y
