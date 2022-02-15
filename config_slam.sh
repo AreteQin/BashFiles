b515630b49bdaf96e733f1617dbd513811d196e1
Your Name <you@example.com>
2022-02-15 13:30:04 -0500

    wsl

---
+++ config_slam.sh
@@ -0,0 +1,30 @@
+cd ~
+git clone https://github.com/strasdat/Sophus.git
+cd Sophus
+mkdir build
+cd build
+cmake ..
+make -j6
+sudo make install
+cd ~
+sudo apt install libsuitesparse-dev libqglviewer-dev-qt5 -y
+git clone https://github.com/RainerKuemmerle/g2o.git
+cd g2o
+mkdir build
+cd build
+cmake ..
+make -j6
+sudo make install
+cd ~
+sudo apt install python3-pip libeigen3-dev -y
+git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin 
+# Install dependencies (as described above, or your preferred method)
+./scripts/install_prerequisites.sh recommended
+# Configure and build
+mkdir build && cd build
+cmake ..
+cmake --build .
+# GIVEME THE PYTHON STUFF!!!! (Check the output to verify selected python version)
+cmake --build . -t pypangolin_pip_install
+sudo apt install liboctomap-dev octovis
--- ubuntu20.sh
+++ ubuntu20.sh
@@ -1,21 +1,5 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo ubuntu-drivers autoinstall
-sudo apt install curl -y
-## Setup edge
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-sudo rm microsoft.gpg
-## Install edge
-sudo apt update
-sudo apt install microsoft-edge-stable -y
-sudo apt install software-properties-common apt-transport-https wget -y
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code -y
-sudo apt-get install ibus-libpinyin -y
-ibus-daemon -d -x -r
 sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential vlc libgoogle-glog-dev -y
 sudo apt update -y
 sudo apt upgrade -y
