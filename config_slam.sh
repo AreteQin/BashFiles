6612c010257ba5d3f6d3f49a2e519ba9b9df25b7
wsl <qinqiaomeng@outlook.com>
2022-02-16 13:26:17 -0500

    token

--- config_slam.sh
+++ config_slam.sh
@@ -7,6 +7,15 @@ cmake ..
 make -j6
 sudo make install
 cd ~
+sudo apt install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgoogle-glog-dev libgtest-dev -y
+git clone https://github.com/ceres-solver/ceres-solver.git
+cd ceres-solver
+mkdir build
+cd build
+cmake ..
+make -j6
+sudo make install
+cd ~
 sudo apt install libsuitesparse-dev libqglviewer-dev-qt5 -y
 git clone https://github.com/RainerKuemmerle/g2o.git
 cd g2o
