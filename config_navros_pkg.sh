229803808fc91ee55c742b9d64ba441e009328f5
Your Name <you@example.com>
2022-02-28 10:46:22 -0500

    navros added

--- config_navros_pkg.sh
+++ config_navros_pkg.sh
@@ -1,3 +1,4 @@
 sudo apt install ros-noetic-pointcloud-to-laserscan ros-noetic-rosbridge-server -y
 sudo apt-get install ros-noetic-controller-manager -y
 sudo apt install ros-noetic-gmapping -y
+sudo apt install ros-noetic-octomap-server -y
