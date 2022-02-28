071b656c2bc25c52c3364a1ed3a8622373b2e49d
Your Name <you@example.com>
2022-02-28 06:06:48 -0500

    created

---
+++ confid_ros_simulation.sh
@@ -0,0 +1,3 @@
+sudo apt install ros-noetic-pointcloud-to-laserscan ros-noetic-rosbridge-server -y
+sudo apt-get install ros-noetic-controller-manager -y
+sudo apt install ros-noetic-gmapping -y
---
+++ install_ros_melodic.sh
@@ -0,0 +1,31 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl -y # if you haven't already installed curl 
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update -y
+
+sudo apt install ros-melodic-desktop-full -y
+
+## To automatically configurate ROS environment while opening a new terminal 
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc 
+
+echo "#export ROS_IP=192.168.99.120" >> ~/.bashrc 
+
+echo "#export ROS_MASTER_URI=http://192.168.99.107:11311" >> ~/.bashrc 
+
+source ~/.bashrc 
+
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+sudo apt-get install ros-melodic-catkin python-catkin-tools
+
+sudo apt install python-rosdep
+
+sudo rosdep init
+
+rosdep update
+
