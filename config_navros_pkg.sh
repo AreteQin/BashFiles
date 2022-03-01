0e00f1d7bab8c226d0d51efdb8c516cac97f8166
qin <qinqiaomeng@outlook.com>
2022-02-28 21:42:04 -0500

    rm navros

--- config_navros_pkg.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install ros-noetic-pointcloud-to-laserscan ros-noetic-rosbridge-server -y
-sudo apt-get install ros-noetic-controller-manager -y
-sudo apt install ros-noetic-gmapping -y
-sudo apt install ros-noetic-octomap-server -y
-sudo apt-get install ros-noetic-octomap-rviz-plugins
