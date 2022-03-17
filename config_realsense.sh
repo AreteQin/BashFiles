6887a84d1449a1e0ccedbe21e6c3897104061fd0
qin <qinqiaomeng@outlook.com>
2022-03-16 20:43:30 -0400

    realsense added

---
+++ config_realsense.sh
@@ -0,0 +1,6 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+sudo apt-get install librealsense2-dkms -y
+sudo apt-get install librealsense2-utils -y
+sudo apt-get install librealsense2-dev -y
+sudo apt-get install librealsense2-dbg -y
