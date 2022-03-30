6a6f5c867778f9c9f7ec90a7040ec99e833b0633
Qin <qinqiaomeng@outlook.com>
2022-03-30 14:49:07 -0400

    badslam updated

--- config_badslam_0.1.sh
+++ config_badslam_0.1.sh
@@ -2,50 +2,49 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential -y
 
-sudo apt-get install ros-noetic-realsense2-camera -y
+# unzip DLib-master.zip
+# cd DLib-master
+# mkdir build
+# cd build
+# cmake .. 
+# make -j8
+# sudo make install
+# cd ..
+# cd ..
 
-unzip DLib-master.zip
-cd DLib-master
+cd ~
+git clone https://github.com/strasdat/Sophus.git
+cd Sophus
 mkdir build
 cd build
-cmake .. 
+cmake ..
 make -j8
 sudo make install
-cd ..
-cd ..
 
-unzip g2o-master.zip
-cd g2o-master
+cd ~
+sudo apt install libsuitesparse-dev libqglviewer-dev-qt5 -y
+git clone https://github.com/RainerKuemmerle/g2o.git
+cd g2o
 mkdir build
 cd build
-cmake .. 
+cmake ..
 make -j8
 sudo make install
-cd ..
-cd ..
 
-unzip opengv-master.zip
-cd opengv-master
-mkdir build
-cd build
-cmake .. 
-make -j8
+cd ~
+git clone https://github.com/laurentkneip/opengv
+cd opengv
+mkdir build && cd build && cmake .. && make -j12
 sudo make install
-cd ..
-cd ..
 
-tar -xvzf zlib-1.2.11.tar.gz 
-cd zlib-1.2.11
-mkdir build
-cd build
-cmake .. 
-make -j8
-sudo make install
-cd ..
-cd ..
+# tar -xvzf zlib-1.2.11.tar.gz 
+# cd zlib-1.2.11
+# mkdir build
+# cd build
+# cmake .. 
+# make -j8
+# sudo make install
 
-tar -xvzf badslam-1.1.0.tar.gz
-cd badslam-1.1.0
-cd opengv-master
+cd ~/Downloads/badslam_without_loop_dector
 mkdir build_RelWithDebInfo && cd build_RelWithDebInfo 
 cmake  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CUDA_FLAGS="-arch=sm_75" .. 
