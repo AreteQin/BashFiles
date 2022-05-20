7f3ffc4e58cffaf585bbd90a7f035d464d178c7a
Qiaomeng Qin <qinqiaomeng@outlook.com>
2022-05-20 11:34:33 -0400

    bad vo created

--- config_badslam_1.0.sh
+++ config_badslam_1.0.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake qt5-default qtcreator libqt5x11extras5-dev libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential -y
+sudo apt-get install libopencv-dev libsuitesparse-dev libxmu-dev libxi-dev libgl-dev libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev cmake libfontconfig1 perl python git libglew-dev libboost-all-dev zip unzip make gcc g++ wget build-essential qt5-default qtcreator ibqt5x11extras5-dev -y
 
 cd ~
 git clone https://github.com/strasdat/Sophus.git
@@ -27,6 +27,6 @@ cd opengv
 mkdir build && cd build && cmake .. && make -j12
 sudo make install
 
-cd ~/Downloads/badslam_without_loop_dector
+cd ~/Downloads/badslam_without_loop_detector
 mkdir build_RelWithDebInfo && cd build_RelWithDebInfo 
 cmake  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CUDA_FLAGS="-arch=sm_75" .. 
