d08c16db91421de956027488657e4b704356e8f3
Qiaomeng Qin <qinqiaomeng@outlook.com>
2023-01-01 21:40:47 -0500

    ros2

---
+++ install_ros2.sh
@@ -0,0 +1,10 @@
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install ros-foxy-desktop python3-argcomplete -y
+sudo apt install ros-dev-tools -y
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc 
