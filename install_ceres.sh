9954eee3ba6c1956c47c81f671d94bc92b1be013
Qiaomeng Qin <qinqiaomeng@outlook.com>
2023-01-29 21:27:31 -0500

    ceres

--- install_ceres.sh
+++ install_ceres.sh
@@ -1,6 +1,6 @@
 cd ~
 sudo apt install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgoogle-glog-dev libgtest-dev -y
-git clone https://github.com/ceres-solver/ceres-solver.git
+git clone https://ceres-solver.googlesource.com/ceres-solver
 cd ceres-solver
 mkdir build
 cd build
