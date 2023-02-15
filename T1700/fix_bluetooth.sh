c3a5e4484e46f89361124424c2d0628af4c8833c
AreteQin <qinqiaomeng@outlook.com>
2023-02-15 13:12:09 -0500

    bluetooth fixed

--- T1700/fix_bluetooth.sh
+++ T1700/fix_bluetooth.sh
@@ -1,8 +1,12 @@
-cd ~
-mkdir Bluetooth
-cd Bluetooth
-wget https://raw.githubusercontent.com/Realtek-OpenSource/android_hardware_realtek/rtk1395/bt/rtkbt/Firmware/BT/rtl8761b_config
-wget https://raw.githubusercontent.com/Realtek-OpenSource/android_hardware_realtek/rtk1395/bt/rtkbt/Firmware/BT/rtl8761b_fw
+# cd ~
+# mkdir Bluetooth
+# cd Bluetooth
+# wget https://raw.githubusercontent.com/Realtek-OpenSource/android_hardware_realtek/rtk1395/bt/rtkbt/Firmware/BT/rtl8761b_config
+# wget https://raw.githubusercontent.com/Realtek-OpenSource/android_hardware_realtek/rtk1395/bt/rtkbt/Firmware/BT/rtl8761b_fw
 
-sudo mv rtl8761b_config /usr/lib/firmware/rtl_bt/rtl8761b_config.bin
-sudo mv rtl8761b_fw /usr/lib/firmware/rtl_bt/rtl8761b_fw.bin
+# sudo mv rtl8761b_config /usr/lib/firmware/rtl_bt/rtl8761b_config.bin
+# sudo mv rtl8761b_fw /usr/lib/firmware/rtl_bt/rtl8761b_fw.bin
+
+cd /lib/firmware/rtl_bt
+sudo ln -s rtl8761b_config.bin rtl8761bu_config.bin
+sudo ln -s rtl8761b_fw.bin rtl8761bu_fw.bin
