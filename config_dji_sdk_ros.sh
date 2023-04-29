766bf168176c1cfe9da7d9e8b345b4a6dcae8405
qin <qinqiaomeng@outlook.com>
2023-04-29 19:53:06 -0400

    osdk

--- config_dji_sdk_ros.sh
+++ config_dji_sdk_ros.sh
@@ -1,1 +1,10 @@
-sudo apt install ros-noetic-nmea-msgs libsdl2-dev
+sudo apt install ros-${ROS_DISTRO}-nmea-msgs libsdl2-dev
+cd ~/Downloads
+git clone https://github.com/AreteQin/dji_osdk_410_opencv4.git
+cd dji_osdk_410_opencv4/
+mkdir build && cd build && cmake .. && make
+sudo make install
+cd ~
+git clone https://github.com/AreteQin/m300_ws.git
+cd m300_ws
+catkin build
