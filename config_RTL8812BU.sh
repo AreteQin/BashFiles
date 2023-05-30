0f3be1c08ce2f27f3df65e9d681beab5b7199f76
qin <qinqiaomeng@outlook.com>
2023-05-30 13:42:56 -0400

    rtl driver

--- config_RTL8812BU.sh
+++ config_RTL8812BU.sh
@@ -4,3 +4,5 @@ git clone -b v5.13.1 https://github.com/fastoe/RTL8812BU.git
 cd RTL8812BU
 make
 sudo make install
+
+#sh -c 'wget deb.trendtechcn.com/install -O /WifiDrivers/install && sh /WifiDrivers/install'
