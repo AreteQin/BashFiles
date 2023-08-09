254dd57346b78563388a26c40d4a5e507d3cc39e
Qin <you@example.com>
2023-08-09 16:41:14 -0400

    rearranged

--- T1700/config_package.sh
+++
@@ -1,6 +0,0 @@
-cd ~
-mkdir build
-cd build
-cmake ..
-make -j2
-sudo make install
--- T1700/config_sophus.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-git clone https://github.com/strasdat/Sophus.git
-cd Sophus
-mkdir build
-cd build
-cmake ..
-make -j2
-sudo make install
