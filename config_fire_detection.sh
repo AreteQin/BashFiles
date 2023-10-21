534ad1e368b931b65829af48dd221adc1b32c2f8
Qiaomeng Qin <you@example.com>
2023-10-21 12:23:42 -0400

    rename and add more dependencies

--- config_fire_detection.sh
+++ config_fire_detection.sh
@@ -2,10 +2,10 @@ sudo apt install python3-pip
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
-pip3 install pandas
-
 git clone https://github.com/ultralytics/ultralytics.git
 
 cd ./ultralytics && pip3 install -r requirements.txt
 
 pip3 install ultralytics
+
+sudo apt-get install ros-melodic-jsk-recognition-msgs -y
