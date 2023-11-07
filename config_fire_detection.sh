dc12d9dcd5d2c0278ff0bf13054bc9a3ec735441
Qiaomeng Qin <you@example.com>
2023-11-07 15:21:00 -0500

    yolo for icrest

--- config_fire_detection.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install python3-pip
-
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-
-git clone https://github.com/ultralytics/ultralytics.git
-
-cd ./ultralytics && pip3 install -r requirements.txt
-
-pip3 install ultralytics
