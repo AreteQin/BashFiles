05348e328bc339eed7f85f57dbf359e07424320d
qin <qinqiaomeng@outlook.com>
2023-12-26 19:22:29 -0500

    updated comments

--- ubuntu20.sh
+++ ubuntu20.sh
@@ -19,8 +19,12 @@ sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code -y
+
+## Chinese input
 sudo apt-get install ibus-libpinyin -y
 ibus-daemon -d -x -r
+
+## Common tools
 sudo apt-get install libopencv-dev libsuitesparse-dev libeigen3-dev cmake python git libboost-all-dev zip unzip make gcc g++ wget build-essential vlc libgoogle-glog-dev libfmt-dev -y
 sudo apt update -y
 sudo apt upgrade -y
