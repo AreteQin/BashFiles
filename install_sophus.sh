7dff8e31e33ca334eceaac813c7ae55f0422500e
qin <qinqiaomeng@outlook.com>
2023-12-28 12:49:01 -0500

    cuda and ceres needed

--- install_sophus.sh
+++ install_sophus.sh
@@ -1,9 +1,33 @@
+echo "Have you installed CUDA and Ceres manually? (y/n) "
+read cuda
+
+case ${cuda} in
+    "y")
+        echo "Downloading Sophus..."
+        ;;
+    "n")
+        echo "Please install CUDA and Ceresfirst"
+        echo "Exiting..."
+        ;;
+esac
+
 cd ~
-sudo apt install libfmt-dev
 git clone https://github.com/strasdat/Sophus.git
 cd Sophus
 mkdir build
 cd build
-cmake ..
-make -j6
-sudo make install
+
+echo "Have you modified the CMakeLists.txt? (y/n) "
+read modified
+
+case ${modified} in
+    "y")
+        sudo apt install libfmt-dev -y
+        cmake ..
+        make -j6
+        sudo make install
+        ;;
+    "n")
+        echo "Exiting..."
+        ;;
+esac
