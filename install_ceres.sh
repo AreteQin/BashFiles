dc99c76d7ea7860541118552d1b6fa95580978f7
qin <qinqiaomeng@outlook.com>
2023-12-28 12:10:11 -0500

    cuda required

--- install_ceres.sh
+++ install_ceres.sh
@@ -1,9 +1,20 @@
-cd ~
-sudo apt install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgoogle-glog-dev libgtest-dev -y
-git clone https://ceres-solver.googlesource.com/ceres-solver
-cd ceres-solver
-mkdir build
-cd build
-cmake ..
-make -j6
-sudo make install
+echo "Have you installed CUDA manually? (y/n) "
+read cuda
+
+case ${cuda} in
+    "y")
+        cd ~
+        sudo apt install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgoogle-glog-dev libgtest-dev -y
+        git clone https://ceres-solver.googlesource.com/ceres-solver
+        cd ceres-solver
+        mkdir build
+        cd build
+        cmake ..
+        make -j6
+        sudo make install
+        ;;
+    "n")
+        echo "Please install CUDA first"
+        echo "Exiting..."
+        ;;
+esac
