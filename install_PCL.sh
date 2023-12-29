aa3ded4a733636b807248a5b4a077118b9e5196c
qin <qinqiaomeng@outlook.com>
2023-12-29 18:49:15 -0500

    created

--- install_PCL.sh
+++ install_PCL.sh
@@ -1,6 +1,7 @@
 sudo apt-get install libflann1.9 libflann-dev libpcl-dev pcl-tools -y
 cd ~
 git clone git@github.com:PointCloudLibrary/pcl.git
+cd pcl
 mkdir release && cd release
 cmake -DCMAKE_BUILD_TYPE=None -DCMAKE_INSTALL_PREFIX=/usr \ -DBUILD_GPU=ON-DBUILD_apps=ON -DBUILD_examples=ON \ -DCMAKE_INSTALL_PREFIX=/usr .. 
 make -j4
