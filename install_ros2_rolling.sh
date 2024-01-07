5065b9d42016044e4667aae342a3ffdd067da2e6
qin <qinqiaomeng@outlook.com>
2024-01-07 16:24:58 -0500

    ros2 rolling

---
+++ install_ros2_rolling.sh
@@ -0,0 +1,28 @@
+locale  # check for UTF-8
+
+sudo apt update -y && sudo apt install locales -y
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+
+sudo apt update -y && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update -y && sudo apt install ros-dev-tools -y
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install ros-rolling-desktop -y
+
+sudo apt install ros-rolling-ros-base -y
+
+echo "source /opt/ros/rolling/setup.bash" >> ~/.bashrc 
