2fc219280f84fad999afbcfd400eaf3a6a0cf4c7
qin <qinqiaomeng@outlook.com>
2024-01-26 19:36:40 -0500

    updated github repo

--- config_RTL8812BU.sh
+++ config_RTL8812BU.sh
@@ -11,14 +11,17 @@ cd ~
 
 case ${system} in
     "1")
-        git clone -b v5.13.1 https://github.com/fastoe/RTL8812BU.git # For x86_64 Ubuntu20
+        #git clone -b v5.13.1 https://github.com/fastoe/RTL8812BU.git # For x86_64 Ubuntu20
+        git clone git@github.com:morrownr/88x2bu-20210702.git
+        cd 88x2bu-20210702/
+        sudo ./install-driver.sh
         ;;
     "2")
         git clone -b v5.6.1 https://github.com/fastoe/RTL8812BU.git # For Jetson Xavier Ubuntu 20
+        cd RTL8812BU
+        make -j2
+        sudo make install
         ;;
 esac
-cd RTL8812BU
-make -j4
-sudo make install
 
 #wget deb.trendtechcn.com/install -O ./install.sh && sh ./install.sh'
