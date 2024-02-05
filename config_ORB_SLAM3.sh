36a64e460f5234b180609cfa76134ab37691a5d9
qin <qinqiaomeng@outlook.com>
2024-02-05 11:05:48 -0500

    for erfan test

---
+++ config_ORB_SLAM3.sh
@@ -0,0 +1,20 @@
+echo "============================================="
+echo export ROS_PACKAGE_PATH=\${ROS_PACKAGE_PATH}:/home/qin/Downloads/ORB_SLAM3_Ubuntu_20/Examples_old/ROS
+echo "============================================="
+echo "Please add the above code into the ~/.bashrc? (y/n) "
+
+gedit ~/.bashrc
+
+read modified
+
+git clone https://github.com/AreteQin/ORB_SLAM3_Ubuntu_20.git
+
+sudo apt install ros-noetic-vision-msgs -y
+
+cd ORB_SLAM3_Ubuntu_20
+
+sudo ldconfig
+
+bash ./build.sh
+
+bash ./build_ros.sh
