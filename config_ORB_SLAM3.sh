fb39ef379be91d51e18694414c38a17aae6a0ae8
qin <qinqiaomeng@outlook.com>
2024-02-05 11:17:42 -0500

    modified folder

--- config_ORB_SLAM3.sh
+++ config_ORB_SLAM3.sh
@@ -1,11 +1,13 @@
 echo "============================================="
-echo export ROS_PACKAGE_PATH=\${ROS_PACKAGE_PATH}:/home/qin/Downloads/ORB_SLAM3_Ubuntu_20/Examples_old/ROS
+echo export ROS_PACKAGE_PATH=\${ROS_PACKAGE_PATH}:~/ORB_SLAM3_Ubuntu_20/Examples_old/ROS
 echo "============================================="
 echo "Please add the above code into the ~/.bashrc? (y/n) "
 
 gedit ~/.bashrc
 
 read modified
+
+cd ~
 
 git clone https://github.com/AreteQin/ORB_SLAM3_Ubuntu_20.git
 
