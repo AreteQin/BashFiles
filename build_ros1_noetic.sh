7fb1b9eb4d8c00586f9c94d50da64d0b744324b3
qin <qinqiaomeng@outlook.com>
2024-02-14 10:17:44 -0500

    files confusing fixed

--- build_ros1_noetic.sh
+++ build_ros1_noetic.sh
@@ -9,59 +9,20 @@ case ${continue} in
         ;;
 esac
 
-locale  # check for UTF-8
+sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstools python3-vcstool build-essential
 
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
+sudo rosdep init
+rosdep update
 
-locale  # verify settings
+mkdir ~/ros_catkin
+cd ~/ros_catkin
 
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+rosinstall_generator desktop --rosdistro noetic --deps --tar > noetic-desktop.rosinstall
+mkdir ./src
+vcs import --input noetic-desktop.rosinstall ./src
 
-sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro noetic -y
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
 
-sudo apt update && sudo apt install -y \
-  libbullet-dev \
-  python3-pip \
-  python3-pytest-cov \
-  ros-dev-tools
-
-# install some pip packages needed for testing
-python3 -m pip install -U \
-  argcomplete \
-  flake8-blind-except \
-  flake8-builtins \
-  flake8-class-newline \
-  flake8-comprehensions \
-  flake8-deprecated \
-  flake8-docstrings \
-  flake8-import-order \
-  flake8-quotes \
-  pytest-repeat \
-  pytest-rerunfailures \
-  pytest
-# install Fast-RTPS dependencies
-sudo apt install --no-install-recommends -y \
-  libasio-dev \
-  libtinyxml2-dev
-# install Cyclone DDS dependencies
-sudo apt install --no-install-recommends -y \
-  libcunit1-dev
-
-mkdir -p ~/ros2_foxy/src
-cd ~/ros2_foxy
-vcs import --input https://raw.githubusercontent.com/ros2/ros2/foxy/ros2.repos src
-
-sudo apt upgrade
-
-sudo rosdep init && rosdep update
-rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-5.3.1 urdfdom_headers"
-
-source ~/.bashrc
-colcon build --symlink-install
+source ~/ros_catkin_ws/install_isolated/setup.bash
--- install_ros1_noetic.sh
+++ install_ros1_noetic.sh
@@ -1,17 +1,42 @@
-sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstools python3-vcstool build-essential
+echo "============================================="
+echo "Installing ROS Noetic, choose platform"
+echo "(1) x86_64 or Jetson"
+echo "(2) Raspberry Pi"
+read system
 
-sudo rosdep init
-rosdep update
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' 
 
-mkdir ~/ros_catkin
-cd ~/ros_catkin
+sudo apt install curl -y # if you haven't already installed curl 
 
-rosinstall_generator desktop --rosdistro noetic --deps --tar > noetic-desktop.rosinstall
-mkdir ./src
-vcs import --input noetic-desktop.rosinstall ./src
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - 
 
-rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro noetic -y
+sudo apt update -y
 
-./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+case ${system} in
+    "1")
+        sudo apt install ros-noetic-desktop-full -y
+        ;;
+    "2")
+        sudo apt install ros-noetic-desktop -y
+        ;;
+esac
 
-source ~/ros_catkin_ws/install_isolated/setup.bash
+## To automatically configurate ROS environment while opening a new terminal 
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc 
+
+# echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc 
+
+echo "#export ROS_IP=192.168.99.120" >> ~/.bashrc 
+
+echo "#export ROS_MASTER_URI=http://192.168.99.107:11311" >> ~/.bashrc 
+
+source ~/.bashrc 
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+
+sudo apt update -y
+
+sudo apt-get install python3-catkin-tools python3-vcstool python3-osrf-pycommon -y
+
+sudo rosdep init && rosdep update
