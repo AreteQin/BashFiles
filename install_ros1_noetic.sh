bee56ff97c616c6683997786bb79f3e2417e6220
qin <qinqiaomeng@outlook.com>
2024-02-14 10:08:19 -0500

    created

---
+++ install_ros1_noetic.sh
@@ -0,0 +1,17 @@
+sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstools python3-vcstool build-essential
+
+sudo rosdep init
+rosdep update
+
+mkdir ~/ros_catkin
+cd ~/ros_catkin
+
+rosinstall_generator desktop --rosdistro noetic --deps --tar > noetic-desktop.rosinstall
+mkdir ./src
+vcs import --input noetic-desktop.rosinstall ./src
+
+rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro noetic -y
+
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+
+source ~/ros_catkin_ws/install_isolated/setup.bash
