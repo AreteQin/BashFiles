cf424e9163220c69a65ce2a8475e9ff1d5d7fd98
qin <qinqiaomeng@outlook.com>
2024-04-29 16:50:16 +0800

    cloudflare

---
+++ install_cloudflare.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+
+sudo apt-get update && sudo apt-get install cloudflare-warp -y
