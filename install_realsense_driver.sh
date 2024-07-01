4aa0c7f4e00e0937bbe7aa58a4697e7ce48ad608
qin <qinqiaomeng@outlook.com>
2024-06-30 21:28:50 -0400

    clear platforms

--- install_realsense_driver.sh
+++ install_realsense_driver.sh
@@ -1,7 +1,7 @@
 echo "============================================="
 echo "Installing Intel Realsense driver, choose platform"
 echo "(1) x86_64"
-echo "(2) QCar"
+echo "(2) QCar or Raspberry Pi"
 read system
 
 case ${system} in
@@ -12,7 +12,7 @@ case ${system} in
         sudo apt install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg -y
         ;;
     "2")
-        # For QCar
+        # For QCar and Raspberry Pi
         sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
         sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo bionic main" -u
         sudo apt-get install librealsense2-utils -y
