c08d430cd19495bcf2994ad0ab23c5e9c0c22faf
qin <qinqiaomeng@outlook.com>
2024-07-04 17:04:48 -0400

    $(lsb_release -cs)

--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@ echo "Add the following to your ~/.bashrc file"
 echo "============================================="
 echo "export PATH=/usr/local/cuda-11.6/bin\${PATH:+:\${PATH}}"
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}"
+
+Var=$(lsb_release -cs)
+echo "$Var"
