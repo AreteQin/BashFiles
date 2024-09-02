e26dc5ce91ea32e1593d8a1bad60dc430eedf9fe
qin <qinqiaomeng@outlook.com>
2024-09-02 14:43:41 -0400

    obs

---
+++ install_obs_studio.sh
@@ -0,0 +1,4 @@
+# It supports Ubuntu 20.04 and newer versions.
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio -y
--- test.sh
+++ test.sh
@@ -4,5 +4,10 @@ echo "============================================="
 echo "export PATH=/usr/local/cuda-11.6/bin\${PATH:+:\${PATH}}"
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}"
 
+# Print the version of Ubuntu
 Var=$(lsb_release -cs)
 echo "$Var"
+
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio -y
