195911f4fc013a597699ebdd2175eedb938a5ed6
qin <qinqiaomeng@outlook.com>
2024-09-28 18:16:26 -0400

    enable vnc

---
+++ enable_vnc.sh
@@ -0,0 +1,39 @@
+sudo vim /etc/gdm3/custom.conf
+
+# Enabling automatic login
+AutomaticLoginEnable = true
+AutomaticLogin = qin
+sudo apt-get install xserver-xorg-video-dummy
+
+sudo vim /etc/X11/xorg.conf
+
+section "Module"
+        Disable "dri"
+        SubSection "extmod"
+                Option "Omit xfree86-dga"
+        EndSubSection
+EndSection
+
+Section "Device"
+    Identifier  "Configured Video Device"
+    Driver      "dummy"
+    # Default is 4MiB, this sets it to 16MiB
+    VideoRam    16384
+EndSection
+
+Section "Monitor"
+    Identifier  "Configured Monitor"
+    HorizSync 31.5-48.5
+    VertRefresh 50-70
+EndSection
+
+Section "Screen"
+    Identifier  "Default Screen"
+    Monitor     "Configured Monitor"
+    Device      "Configured Video Device"
+    DefaultDepth 24
+    SubSection "Display"
+    Depth 24
+    Modes "1024x800"
+    EndSubSection
+EndSection
