91a2a3283b47f4300b565bfc098a11f631b82e33
qin <qinqiaomeng@outlook.com>
2024-12-20 18:32:38 -0500

    not tested

--- install_ceres.sh
+++ install_ceres.sh
@@ -3,21 +3,16 @@ read cuda
 
 case ${cuda} in
     "y")
-        cd ~
-        sudo apt install cmake libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev -y
-        git clone https://github.com/ceres-solver/ceres-solver.git -b 2.2.0
-        cd ceres-solver
-        mkdir ceres-bin
-        cd ceres-bin
-        cmake ..
-        make -j3
-        # git clone https://ceres-solver.googlesource.com/ceres-solver
+        # cd ~
+        # sudo apt install cmake libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev -y
+        # git clone https://github.com/ceres-solver/ceres-solver.git -b 2.2.0
         # cd ceres-solver
-        # mkdir build
-        # cd build
+        # mkdir ceres-bin
+        # cd ceres-bin
         # cmake ..
-        # make -j6
-        sudo make install
+        # make -j3
+        # sudo make install
+        sudo apt install libceres-dev
         ;;
     "n")
         echo "Please install CUDA first"
