5114bac5dc26dc4f5811575ce18f41e83202cf9f
qin <qinqiaomeng@outlook.com>
2025-01-01 17:44:00 -0500

    tested on ubuntu 20

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,25 +1,25 @@
 # CUDA
 
-# echo "============================================="
-# echo "Have you downloaded CUDA installer? [y/n]"
-# read cuda
+echo "============================================="
+echo "Have you downloaded CUDA installer? [y/n]"
+read cuda
 
-# cd ~/Downloads
+cd ~/Downloads
 
-# case ${cuda} in
-#     "n")
-#         echo "Downloading..."
-#         wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-#         ;;
-# esac
+case ${cuda} in
+    "n")
+        echo "Downloading..."
+        wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+        ;;
+esac
 
-# sudo sh cuda_*.run
+sudo sh cuda_*.run
 
-# # Append CUDA paths to ~/.bashrc
-# echo "export PATH=/usr/local/cuda-11.8/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
-# echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ~/.bashrc
+# Append CUDA paths to ~/.bashrc
+echo "export PATH=/usr/local/cuda-11.8/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ~/.bashrc
 
-# # Reload ~/.bashrc to apply changes
-# source ~/.bashrc
+# Reload ~/.bashrc to apply changes
+source ~/.bashrc
 
-sudo apt install nvidia-cuda-toolkit nvidia-cuda-toolkit-gcc -y
+# sudo apt install nvidia-cuda-toolkit nvidia-cuda-toolkit-gcc -y # too old CUDA version
