40ac6aaa220adb6a7fb666aedaac140790b6564a
qin <qinqiaomeng@outlook.com>
2025-03-17 20:26:16 -0400

    for icrest

---
+++ install_absl.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+codename=$(lsb_release -c | awk '{print $2}')
+
+case $codename in
+"impish")
+    echo "Ubuntu 22.04 LTS"
+    sudo apt install libabsl-dev -y
+    exit
+    ;;
+"focal")
+    echo "Ubuntu 20.04 LTS"
+    ;;
+esac
+
+git clone https://github.com/abseil/abseil-cpp.git
+cd abseil-cpp
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
+make -j2
+sudo make install
