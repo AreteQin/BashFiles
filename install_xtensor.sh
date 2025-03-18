3bc294667b0c5e91cfc76945110eab1617b741af
qin <qinqiaomeng@outlook.com>
2025-03-17 21:20:46 -0400

    for icrest

--- install_xtensor.sh
+++ install_xtensor.sh
@@ -1,4 +1,12 @@
 cd ~
+git clone https://github.com/xtensor-stack/xtl.git
+cd xtl
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
+make -j$(nproc)
+sudo make install
+
+cd ~
 git clone https://github.com/xtensor-stack/xtensor.git
 cd xtensor
 mkdir build && cd build
